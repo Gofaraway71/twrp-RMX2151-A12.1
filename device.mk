08ee2fa370b6da4cd2506e6976205cb77b0d37e0
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2022-08-25 06:53:44 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ PRODUCT_PACKAGES += \
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 # Health
