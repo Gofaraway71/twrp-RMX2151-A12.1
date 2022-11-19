98247ae339690ba818abaa8acb1f6850a00c54c8
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2022-11-19 19:44:11 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,10 @@ DEVICE_PATH := device/realme/RMX2151
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+# Broken* [HAX]
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
