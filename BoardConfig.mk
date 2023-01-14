d0fee8c3227e28a547de1f7b2c95e079557c53fa
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2023-01-14 19:06:19 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone4/temp
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/power_supply/battery/temp
 TW_INCLUDE_LOGICAL := my_product my_engineering my_company my_carrier my_region my_heytap my_stock my_preload my_manifest
 
 # Debug flags
