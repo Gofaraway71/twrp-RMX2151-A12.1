d927d00138825d66abeed29215891d0cb7b8917f
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2023-02-06 08:23:28 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,7 +14,6 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31 \
     ro.bootimage.build.date.utc=0 \
     ro.build.date.utc=0
 
