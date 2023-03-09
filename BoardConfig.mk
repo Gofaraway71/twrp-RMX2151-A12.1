cbb64de4fa7c0161c30e1cbab230fe2f5789c255
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2023-03-09 12:25:12 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_USES_UEFI := true
 TARGET_BOARD_PLATFORM := mt6785
 
 # Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive loop.max_part=70
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 loop.max_part=70
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=bootloader
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x40078000
